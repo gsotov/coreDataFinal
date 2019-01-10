f1fda526cada295d92c6d63ecb84cdd3dd765d4f
Tecnova <tecnova@MacBook-Pro-de-Tecnova.local>
2019-01-10 11:39:10 -0300

    estructura proyecto

---
+++ coreDataFinal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coreDataFinal
+//
+//  Created by Tecnova on 1/10/19.
+//  Copyright © 2019 Gabriel Soto Valenzuela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
